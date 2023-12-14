607f6d27ca7683698292ec017879429ec74d07be
Patrickming <patrickmingx@gmail.com>
2023-12-14 22:44:57 +0800

    commit Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar.js';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [walletAddress, setWalletAddress] = useState("");
+  //调用函数
+  useEffect(() => {
+    // getWalletAddress();
+  });
+
+  async function getWalletAddress() {
+    if (window.ethereum) {
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const account = accounts[0];
+      setWalletAddress(account);
+    } else {
+      alert('Please install Metamask');
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Navbar onconnectWallet={getWalletAddress} walletAddress={walletAddress}/>
+      <p>Your Address: {walletAddress}</p>
     </div>
   );
 }
